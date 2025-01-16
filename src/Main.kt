715acb01ab890e55b5c4278db116b9652f98e600
irolram <ivanr9016@gmail.com>
2025-01-16 20:38:29 +0100

    cambios

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+class Coche(var color:String,
+            marca:String,
+            var numeroCaballos: Int,
+            var numeroPuertas: Int,
+            modelo: String,
+            matricula:String
+            ){
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val modelo: String = modelo
+        get() = field.replaceFirstChar { it.uppercase() }
+
+
+    val marca: String = marca
+        get()=  field.lowercase().replaceFirstChar { it.uppercase() }
+
+
+
+    private var matricula:String = ""
+        set(value) {
+            require(value.length == 7){ "La matrícula debe tener 7 caracteres" }
+            field = value
+        }
+
+
+    override fun toString(): String {
+        return "color: $color, marca: $marca, modelo: $modelo, numero de caballos: $numeroCaballos, número de puertas: $numeroPuertas, matricula: $matricula"
     }
+    init {
+        require(marca.isNullOrBlank() ){ "marca no puede ser blanco ni vacio" }
+        require(modelo.isNullOrBlank()){ "Modelo no puede ser blanco ni vacio" }
+        require(color != null)
+    }
+
+}
+
+fun main(){
+
+
 }
