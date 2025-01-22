2570fe2277b902748e10fa300014bfc40b426cac
irolram <rrivan@iesrafaelalberti.es>
2025-01-22 03:16:49 +0100

    hecho

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,4 @@
-class Coche(var color:String,
+class Coche(color:String,
             marca:String,
             var numeroCaballos: Int,
             var numeroPuertas: Int,
@@ -14,8 +14,15 @@ class Coche(var color:String,
         get()=  field.lowercase().replaceFirstChar { it.uppercase() }
 
 
+    var color:String = color
+        set(value) {
+            require(!value.isNullOrEmpty()) { "El color no puede ser nulo" }
+            field = value
+        }
 
-    private var matricula:String = ""
+
+
+    var matricula:String = matricula
         set(value) {
             require(value.length == 7){ "La matrícula debe tener 7 caracteres" }
             field = value
@@ -26,14 +33,28 @@ class Coche(var color:String,
         return "color: $color, marca: $marca, modelo: $modelo, numero de caballos: $numeroCaballos, número de puertas: $numeroPuertas, matricula: $matricula"
     }
     init {
-        require(marca.isNullOrBlank() ){ "marca no puede ser blanco ni vacio" }
-        require(modelo.isNullOrBlank()){ "Modelo no puede ser blanco ni vacio" }
-        require(color != null)
+        require(!marca.isNullOrBlank() ){ "marca no puede ser blanco ni vacio" }
+        require(!modelo.isNullOrBlank()){ "Modelo no puede ser blanco ni vacio" }
+        require(color != null) { "El color no puede ser nulo" }
+        require(numeroCaballos in 70..700){"El rango de caballos no es valido"}
+        require(numeroPuertas in 3..5){"El rango de puertas no es valido"}
+        require(matricula.length == 7){ "La matrícula debe tener 7 caracteres" }
     }
 
 }
 
 fun main(){
 
+    /*
+
+    val coche2 = Coche("verde", "", 100, 3, "", "DEF4568")
+    println(coche2)
+
+    val coche3 = Coche("azul", "ford", 400, 7, "Focus", "GHI7898")
+    println(coche3)
+    val coche4 = Coche("azul", "ford", 400, 4, "Focus", "G7898")
+      */
+    val coche1 = Coche("Rojo", "bmv", 100, 4, "2008", "ABC1234")
+    coche1.color = ""
 
 }
